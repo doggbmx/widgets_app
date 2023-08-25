cc48043a1f3f4804938b1ba994ba8dcf994fff8e
doggbmx <manuelorticosta@gmail.com>
2023-08-25 11:41:18 -0400

    custom button added

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -80,7 +80,7 @@ class _ButtonsView extends StatelessWidget {
               icon: const Icon(Icons.priority_high_sharp),
               label: const Text('Text Icon'),
             ),
-            // TODO: CUSTOM BUTTON
+            const CustomButton(),
             const IconButton(
               onPressed: null,
               icon: Icon(Icons.electric_bike),
@@ -94,6 +94,32 @@ class _ButtonsView extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'HolaMundo',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
         ),
       ),
     );
