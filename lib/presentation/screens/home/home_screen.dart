f0f69ee5e66b99fd39b7cab6eb21af2821549990
doggbmx <manuelorticosta@gmail.com>
2023-08-25 10:55:32 -0400

    go router configured

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -53,7 +54,9 @@ class _CustomListTile extends StatelessWidget {
         //     builder: (context) => const ButtonsScreen(),
         //   ),
         // )
-        Navigator.pushNamed(context, menuItem.link)
+        // Navigator.pushNamed(context, menuItem.link)
+
+        context.push(menuItem.link)
       },
     );
   }
