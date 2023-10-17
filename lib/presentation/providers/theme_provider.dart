4fcf858c4764391dbcf9c34eac45dbd562846fbc
doggbmx <manuelorticosta@gmail.com>
2023-10-17 11:07:12 -0300

    themeNotifierProvider created

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -11,3 +11,19 @@ final selectedColorProvider = StateProvider<int>((ref) => 0);
 
 // Listado de colores inmutables
 final colorListProvider = Provider(((ref) => colorList));
+
+// Un objeto de tipo AppTheme (custom)
+
+final themeNotifierProvider = StateNotifierProvider<ThemeNotifier, AppTheme>(
+  (ref) => ThemeNotifier(),
+);
+
+// Controller o Notifier
+class ThemeNotifier extends StateNotifier<AppTheme> {
+  // State = Estado = new AppTheme();
+  ThemeNotifier() : super(AppTheme());
+
+  void toggleDarkMode() {}
+
+  void changeColorInder(int colorIndex) {}
+}
