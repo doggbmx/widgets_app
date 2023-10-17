11f2485c6a2f5ace9b72a044aeea6b3f062d0dd2
doggbmx <manuelorticosta@gmail.com>
2023-10-17 11:07:22 -0300

    themeNotifierProvider implemented

--- lib/main.dart
+++ lib/main.dart
@@ -17,15 +17,13 @@ class MainApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isDarkMode = ref.watch(themeProvider);
-    final int selectedColor = ref.watch(selectedColorProvider);
+    // final isDarkMode = ref.watch(themeProvider);
+    // final int selectedColor = ref.watch(selectedColorProvider);
+    final AppTheme appTheme = ref.watch(themeNotifierProvider);
     return MaterialApp.router(
       title: 'Flutter Widgets',
       routerConfig: appRouter,
-      theme: AppTheme(
-        selectedColor: selectedColor,
-        isDarkMode: isDarkMode,
-      ).getTheme(),
+      theme: appTheme.getTheme(),
       debugShowCheckedModeBanner: false,
     );
   }
