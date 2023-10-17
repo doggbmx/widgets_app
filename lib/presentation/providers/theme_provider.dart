e719c714a106c0728944c9621681286d1a1d89aa
doggbmx <manuelorticosta@gmail.com>
2023-10-17 10:03:50 -0300

    add colorListProvider

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -1,5 +1,9 @@
 // Estado => bool isDarkModeProvider
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
 
 final themeProvider = StateProvider<bool>((ref) => false);
+
+// Listado de colores inmutables
+final colorListProvider = Provider(((ref) => colorList));
