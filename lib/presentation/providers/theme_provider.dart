e1671a9260c94a5ff5345488b1664eab2c563a16
doggbmx <manuelorticosta@gmail.com>
2023-10-17 10:58:12 -0300

    add more states to provider

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -3,7 +3,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
 
+// un simple booleano
 final themeProvider = StateProvider<bool>((ref) => false);
+
+// un simple int
+final selectedColorProvider = StateProvider<int>((ref) => 0);
 
 // Listado de colores inmutables
 final colorListProvider = Provider(((ref) => colorList));
